421dffdc63aead8d440cef0c83c4a02dcdb7038c
Oltersdorf Benedikt <1810653774@stud.fh-kufstein.ac.at>
2018-11-08 09:43:48 +0100

    segrundlagen

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String name = "Benedikt Oltersdorf";
+        System.out.println(name);
+        System.out.println();;
+    }
+
+
+
+
+
+}
