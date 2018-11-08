8b85a16bddacfed3052b28a08a3e781790698e37
Oltersdorf Benedikt <1810653774@stud.fh-kufstein.ac.at>
2018-11-08 10:49:41 +0100

    Aufgabe3ue

---
+++ src/Aufgabe3ue/aufgabe3ue.java
@@ -0,0 +1,16 @@
+package Aufgabe3ue;
+
+public class aufgabe3ue {
+
+    public static void main(String[] args) {
+
+        String name = "Bene";
+        System.out.println(name);
+        int alter = 24;
+        System.out.println(alter);
+    }
+
+
+
+
+}
