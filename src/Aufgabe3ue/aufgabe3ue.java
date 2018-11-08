1c08a066a1532db785aca81046769f9a44c0b624
Oltersdorf Benedikt <1810653774@stud.fh-kufstein.ac.at>
2018-11-08 11:41:50 +0100

    Aufgabe6ue

--- src/Aufgabe3ue/aufgabe3ue.java
+++ src/Aufgabe3ue/aufgabe3ue.java
@@ -12,6 +12,17 @@ public class aufgabe3ue {
         // final Konstante
         final String ort = "Putzbrunn";
 
+        String Satz  = "JavaverwendetfürStrings\"doppelteAnführungszeichen\"undeinzelneAnführungszeichen\n wie 'c' um einzelne Zeichen zu definieren.";
+        System.out.println(Satz);
+
+        String Satz2 = "Durch \\ kann man auch den Backslash selbst beliebig oft ausgeben: \\\\\\\\";
+        System.out.println(Satz2);
+
+        String Satz3 = "Ich verwende den\b Backspace, die Newline\n und den \tTabulator";
+        System.out.println(Satz3);
+
+        String Satz4 = "Durch den Wagenruecklauf wird die Zeile zurueckgesetzt und der Teil nach dem \r ueberschreibt\n diebestehendeZeile. \f istinderKonsolenichtbemerkbar,daeseinenSeitenumbruch erzwingt.";
+        System.out.println(Satz4);
 
     }
 
