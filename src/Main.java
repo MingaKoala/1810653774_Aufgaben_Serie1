b41bac75ee84218a51c065cf11e1b5efd86b96c1
Oltersdorf Benedikt <1810653774@stud.fh-kufstein.ac.at>
2018-11-08 10:36:55 +0100

    Aufgabe2 Übung

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-
+// Erstellt meine Klasse
 public class Main {
-
+//Erstellt die Main Methode
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");         // Eingabe und Ausgabe
         String name = "Benedikt Oltersdorf";
-        System.out.println(name);
-        System.out.println();;
+        System.out.println(name);                   // Eingabe und Ausgabe Name
+        System.out.println();                      // Eingabe und Ausgabe der Main methode
     }
 
 
--- src/fhkufstein/at/ac/seuebung.java
+++ src/fhkufstein/at/ac/seuebung.java
@@ -1,4 +1,13 @@
-package fhkufstein.at.ac;
+package fhkufstein.at.ac;   // In welchen Ordner die Datei liegt
 
-public class seuebung {
+
+public class seuebung { // Öffentliche Klasse
+
+    public static void main(String[] args) {    //eröffnet die Main methode
+
+        String schule = "FH Kufstein Tirol";    // eingabe vom Begriff mit Buchstaben mit hilfe eines Datentyps
+        System.out.println(schule);             // Ausgabe des Begriffes
+
+    }
+
 }
